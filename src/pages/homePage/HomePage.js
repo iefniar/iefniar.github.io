6420861f586c576b7f6021004fdd3d8196130b59
iefniar <fejeffrey.uv@gmail.com>
2024-03-15 13:07:17 -0300

    hero section

--- src/pages/homePage/HomePage.js
+++ src/pages/homePage/HomePage.js
@@ -5,6 +5,7 @@ import About from './pageSections/About';
 import Projects from './pageSections/Projects';
 import Contact from './pageSections/Contact';
 import Logo from '../../icons/Logo.svg';
+import Button from '../../components/Button';
 
 const HomePage = () => {
   const aboutRef = useRef(null);
@@ -26,30 +27,40 @@ const HomePage = () => {
   return (
     <div className={classes['homePage-container']}>
       <div className={classes['navBar-container']}>
-        <div className={classes['navBar-container-links']}>
-          <button className={classes['navBar-btn']} onClick={handleClickAbout}>
-            <h4>About</h4>
-          </button>
-        </div>
-        <div className={classes['navBar-container-links']}>
-          <button className={classes['navBar-btn']} onClick={handleClickProjects}>
-            <h4>Projects</h4>
-          </button>
-        </div>
-        <div className={classes['navBar-container-links']}>
-          <button className={classes['navBar-btn']} onClick={handleClickContact}>
-            <h4>Contact</h4>
-          </button>
-        </div>
-        <div className={classes['navBar-container-links']}>
-          <button className={classes['navBar-btn']} >
-            <img src={Logo} alt="logo" />
-          </button>
+        <div className={classes['navBar-left']} />
+        <div className={classes['navBar-right']}>
+          <div className={classes['navBar-container-links']}>
+            <button className={classes['navBar-btn']} onClick={handleClickAbout}>
+              <h4>About</h4>
+            </button>
+          </div>
+          <div className={classes['navBar-container-links']}>
+            <button className={classes['navBar-btn']} onClick={handleClickProjects}>
+              <h4>Projects</h4>
+            </button>
+          </div>
+          <div className={classes['navBar-container-links']}>
+            <button className={classes['navBar-btn']} onClick={handleClickContact}>
+              <h4>Contact</h4>
+            </button>
+          </div>
+          <div className={classes['navBar-container-links']}>
+            <button className={classes['navBar-btn']} >
+              <img src={Logo} alt="logo" />
+            </button>
+          </div>
         </div>
       </div>
-
       <div className={classes['hero-section']}>
         <Hero/>
+      </div>
+      <div className={classes['call-to-action-area']}>
+        <div className={classes['button-area']}>
+          <Button text="get started" />
+        </div>
+        <div className={classes['cta-text-area']}>
+          Craft a user experience that converts
+        </div>
       </div>
       <div className={classes['about-section']}>
         <About ref={aboutRef}/>
@@ -60,8 +71,6 @@ const HomePage = () => {
       <div className={classes['contact-section']}>
         <Contact ref={contactRef} />
       </div>
-      
-      
     </div>
   );
 }
--- src/pages/homePage/pageSections/Hero.js
+++ src/pages/homePage/pageSections/Hero.js
@@ -1,14 +1,13 @@
 import classes from './Hero.module.css';
 
-
 const Hero = () => {
   return (
-    <div className={classes['text-container']}>
-        <div className={classes['text-1']}>modern uis</div>
-        <div className={classes['text-2']}>that</div>
-        <div className={classes['text-3']}>convert</div>
-        <div className={classes['text-4']}>visitors into</div>
-        <div className={classes['text-5']}>customers</div>
+    <div className={classes['main-container']}>
+        <div className={`${classes['text-type-1']} ${classes['text-1']}`}>modern uis</div>
+        <div className={`${classes['text-type-2']} ${classes['text-2']}`}>that</div>
+        <div className={`${classes['text-type-2']} ${classes['text-3']}`}>convert</div>
+        <div className={`${classes['text-type-1']} ${classes['text-4']}`}>visitors into</div>
+        <div className={`${classes['text-type-2']} ${classes['text-5']}`}>customers</div>
     </div>
   );
 }
