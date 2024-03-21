8d77ce3193e881c116dac3c90d19bce3dc8be16a
iefniar <fejeffrey.uv@gmail.com>
2024-03-21 02:10:23 -0300

    scroll based animations

--- src/pages/homePage/HomePage.js
+++ src/pages/homePage/HomePage.js
@@ -1,13 +1,28 @@
 import classes from './HomePage.module.css';
-import { useRef } from 'react';
+import { useRef, useEffect  } from 'react';
 import Hero from './pageSections/Hero';
 import About from './pageSections/About';
 import Projects from './pageSections/Projects';
 import Contact from './pageSections/Contact';
 import Logo from '../../icons/Logo.svg';
 import Button from '../../components/Button';
+import Lenis from '@studio-freight/lenis';
+import { useTransform, useScroll, motion } from 'framer-motion';
 
 const HomePage = () => {
+  useEffect(() => {
+    const lenis = new Lenis();
+
+    function raf(time) {
+    lenis.raf(time)
+    requestAnimationFrame(raf)
+    }
+
+    requestAnimationFrame(raf)
+
+  }, []);
+  
+
   const aboutRef = useRef(null);
   const projectsRef = useRef(null);
   const contactRef = useRef(null);
@@ -23,10 +38,62 @@ const HomePage = () => {
   const handleClickContact = () => {
     contactRef.current?.scrollIntoView({ behavior: 'smooth' });
   };
+
+  const mainContainer = useRef(null);
+  const { scrollYProgress: scrollYProgressMainContainer } = useScroll({
+      target: mainContainer,
+      offset: ['start end', 'end start']
+  });
+
+  const y1MainContainer = useTransform(scrollYProgressMainContainer, [0, 1], [0, window.innerHeight]);
+  const y2MainContainer = useTransform(scrollYProgressMainContainer, [0, 1], [0, window.innerHeight*1.01]);
+  const y3MainContainer = useTransform(scrollYProgressMainContainer, [0, 1], [0, window.innerHeight*1.1]);
+  const y4MainContainer = useTransform(scrollYProgressMainContainer, [0, 1], [0, window.innerHeight*(-1)]);
   
+  const aboutContainer = useRef(null);
+  const { scrollYProgress: scrollYProgressAboutContainer } = useScroll({
+      target: aboutContainer,
+      offset: ["0 1", "0.33 1"]
+  });
+
+  const y1AboutContainer = useTransform(scrollYProgressAboutContainer, [0, 1], [0, window.innerHeight*1.01]);
+  const scaleProgressAboutContainer = useTransform(scrollYProgressAboutContainer, [0, 1], [0.8, 1]);
+  const opacityProgressAboutContainer = useTransform(scrollYProgressAboutContainer, [0, 1], [0.5, 1]);
+
+  const projectsContainer = useRef(null);
+  const { scrollYProgress: scrollYProgressProjectsContainer } = useScroll({
+      target: projectsContainer,
+      offset: ["0 1", "0.33 1"]
+  });
+
+  const scaleProgressProjectsContainer = useTransform(scrollYProgressProjectsContainer, [0, 1], [0.9, 1]);
+  const opacityProgressProjectsContainer = useTransform(scrollYProgressProjectsContainer, [0, 1], [0.4, 1]);
+
+  const contactContainer = useRef(null);
+  const { scrollYProgress: scrollYProgressContactContainer } = useScroll({
+      target: contactContainer,
+      offset: ["0 1", "0.33 1"]
+  });
+
+  const scaleProgressContactContainer = useTransform(scrollYProgressContactContainer, [0, 1], [0.9, 1]);
+  const opacityProgressContactContainer = useTransform(scrollYProgressContactContainer, [0, 1], [0.7, 1]);
+
   return (
-    <div className={classes['homePage-container']}>
-      <div className={classes['navBar-container']}>
+    <div ref={mainContainer} className={classes['homePage-container']}>
+      <motion.div className={classes['navBar-container']}
+        initial={{
+          scale: 1.1,
+          opacity: 0,
+        }}
+        animate={{
+          scale: 1.0,
+          opacity: 1,
+          transition: {
+              duration: 1,
+              delay: 1
+          }
+        }}
+      >
         <div className={classes['navBar-left']} />
         <div className={classes['navBar-right']}>
           <div className={classes['navBar-container-links']}>
@@ -50,29 +117,66 @@ const HomePage = () => {
             </button>
           </div>
         </div>
-      </div>
-      <div className={classes['hero-section']}>
+      </motion.div>
+      <motion.div className={classes['hero-section']}
+        initial={{
+          scale: 1.1,
+          opacity: 0,
+        }}
+        animate={{
+          scale: 1.0,
+          opacity: 1,
+          transition: {
+              duration: 1,
+              delay: 2
+          }
+        }}
+      >
         <Hero/>
-      </div>
-      <div className={classes['call-to-action-area']}>
+      </motion.div>
+      <motion.div className={classes['call-to-action-area']}
+        initial={{
+          scaleX: 0.8,
+          opacity: 0,
+        }}
+        animate={{
+          opacity: 1,
+          scaleX: 1.0,
+          transition: {
+            duration: 2,
+            delay: 1
+          }
+        }}
+      >
         <div className={classes['button-area']}>
           <Button text="get started" />
         </div>
         <div className={classes['cta-text-area']}>
           Craft a user experience that converts
         </div>
-      </div>
+      </motion.div>
       <div className={classes['about-projects-container']}>
-        <div className={classes['about-section']}>
-          <About ref={aboutRef}/>
+        <div ref={aboutContainer}>
+          <motion.div className={classes['about-section']}
+                      style={{opacity: opacityProgressAboutContainer, y: scrollYProgressAboutContainer, scale: scaleProgressAboutContainer}}
+          >
+            <About ref={aboutRef}/>
+          </motion.div>
         </div>
-        <div className={classes['projects-section']}>
-          <Projects ref={projectsRef} />
+        <div ref={projectsContainer}>
+          <motion.div className={classes['projects-section']}
+            style={{y: scrollYProgressProjectsContainer, scale: scaleProgressProjectsContainer}}
+          >
+            <Projects ref={projectsRef} />
+          </motion.div>
         </div>
       </div>
-      
-      <div className={classes['contact-section']}>
-        <Contact ref={contactRef} />
+      <div ref={contactContainer}>
+        <motion.div className={classes['contact-section']}
+          style={{opacity: opacityProgressContactContainer, y: scrollYProgressContactContainer, scale: scaleProgressContactContainer}}
+        >
+          <Contact ref={contactRef} />
+        </motion.div>
       </div>
     </div>
   );
--- src/pages/homePage/pageSections/About.js
+++ src/pages/homePage/pageSections/About.js
@@ -1,11 +1,21 @@
-import { forwardRef } from "react";
+import { forwardRef, useRef } from "react";
 import classes from './About.module.css';
 import MongoDBIcon from '../../../icons/devicon_mongodb.svg';
 import ReactIcon from '../../../icons/devicon_react.svg';
 import ExpressIcon from '../../../icons/devicon_express.svg';
 import NodejsIcon from '../../../icons/vscode-icons_file-type-node.svg';
+import { useTransform, useScroll, motion } from 'framer-motion';
 
 const About = (props, ref) => {
+  const iconsContainer = useRef(null);
+  const { scrollYProgress: scrollYProgressIconsContainer } = useScroll({
+      target: iconsContainer,
+      offset: ["1.33 1.33", "1 1"]
+  });
+
+  const scaleProgressIconsContainer = useTransform(scrollYProgressIconsContainer, [0, 1], [0.9, 1]);
+  const opacityProgressIconsContainer = useTransform(scrollYProgressIconsContainer, [0, 1], [0.8, 1]);
+
   return (
     <div className={classes['about-me-container']} ref={ref}> 
       <div className={`${classes['align-right']} ${classes['first-text']}`}>
@@ -20,13 +30,15 @@ const About = (props, ref) => {
         <div className={`${classes['text-type-2']} ${classes['text-5']}`}>Mern</div>
         <div className={classes['text-type-1']}>stack</div>
       </div>
-      <div className={classes['icons-area']}>
-        <div className={classes['first-group']}>
+      <div ref={iconsContainer} className={classes['icons-area']}>
+        <motion.div className={classes['first-group']}
+                    style={{opacity: opacityProgressIconsContainer, y: scrollYProgressIconsContainer, scale: scaleProgressIconsContainer}}
+        >
           <img src={MongoDBIcon} alt="MongoDB icon" />
           <h4>MongoDB</h4>
           <img src={ReactIcon} alt="React.js icon" />
           <h4>React.js</h4>
-        </div>
+        </motion.div>
         <div className={classes['second-group']}>
           <img src={ExpressIcon} alt="Express.js icon" />
           <h4>Express.js</h4>
