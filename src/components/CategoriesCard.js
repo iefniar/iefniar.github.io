2ee17c01bb54ce9b4b8672337a3ee53ee560007d
iefniar <fejeffrey.uv@gmail.com>
2024-05-27 21:09:54 -0300

    using framer-motion on ecommerce homepage

--- src/components/CategoriesCard.js
+++ src/components/CategoriesCard.js
@@ -1,9 +1,9 @@
 import classes from './CategoriesCard.module.css';
 import ECommerceButton from './ECommerceButton';
 
-const CategoriesCard = ({imagePath, headline, btnText, btnPath}) => {
+const CategoriesCard = ({id, imagePath, headline, btnText, btnPath}) => {
   return (
-    <div className={classes['container']}>
+    <div className={classes['container']} id={id}>
         <img src={imagePath} alt='small-preview' />
         <div className={classes['group-container']}>
           <h1>{headline}</h1>
--- src/pages/ecommercePage/pageSections/Home.js
+++ src/pages/ecommercePage/pageSections/Home.js
@@ -5,7 +5,7 @@ import ECommerceButton from '../../../components/ECommerceButton';
 import { ArrowCircleDownOutlined, ArrowDropDownOutlined } from '@mui/icons-material';
 import gsap from 'gsap';
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
-import { useTransform, useScroll, motion } from 'framer-motion';
+import { useTransform, useScroll, motion, useAnimate, useInView } from 'framer-motion';
 
 const Home = () => {
   const middleSectionContainer = useRef(null);
@@ -31,6 +31,44 @@ const Home = () => {
   const y1Child2MiddleBottomSectionContainer = useTransform(scrollYProgressMiddleBottomSectionContainer, [0, 1], [window.innerHeight*0.8, 1]);
   const scaleProgressChild2MiddleBottomSectionContainer = useTransform(scrollYProgressMiddleBottomSectionContainer, [0, 1], [0.8, 1]);
   const opacityProgressChild2MiddleBottomSectionContainer = useTransform(scrollYProgressMiddleBottomSectionContainer, [0, 1], [0, 1]);  
+
+  const [cardsScope, animate] = useAnimate();
+  const isInView = useInView(cardsScope);
+  
+  useEffect(() => {
+    const animationSequenceFn = async () => {
+      if (isInView) {
+        await animate("#card-1", { y: '-4rem', opacity: 1 }, { duration: 0.3 });
+        await animate("#card-1", { y: '-3rem' }, { duration: 0.3 });
+        await animate("#card-2", { y: '-4.1rem', opacity: 1 }, { duration: 0.3 });
+        await animate("#card-2", { y: '-3rem' }, { duration: 0.3 });
+        await animate("#card-3", { y: '-4.2rem', opacity: 1 }, { duration: 0.3 });
+        await animate("#card-3", { y: '-3rem' }, { duration: 0.3 });
+        await animate("#card-4", { y: '-4rem', opacity: 1 }, { duration: 0.3 });
+        await animate("#card-4", { y: '-3rem' }, { duration: 0.3 });
+        await animate("#card-6", { y: '-4.2rem', opacity: 1 }, { duration: 0.3 });
+        await animate("#card-6", { y: '-3rem' }, { duration: 0.3 });
+        await animate("#card-5", { y: '-4.1rem', opacity: 1 }, { duration: 0.3 });
+        await animate("#card-5", { y: '-3rem' }, { duration: 0.3 });
+        
+       
+      }
+      else{
+        await animate("#card-1", { y: '4rem', opacity: 0 }, { duration: 0 });
+        await animate("#card-2", { y: '4rem', opacity: 0 }, { duration: 0 });
+        await animate("#card-3", { y: '4rem', opacity: 0 }, { duration: 0 });
+        await animate("#card-4", { y: '4rem', opacity: 0 }, { duration: 0 });
+        await animate("#card-5", { y: '4rem', opacity: 0 }, { duration: 0 });
+        await animate("#card-6", { y: '4rem', opacity: 0 }, { duration: 0 });
+     
+
+      }
+
+    }
+
+    animationSequenceFn();
+     
+  }, [isInView]);
 
   //gsap code start
   gsap.registerPlugin(ScrollTrigger);
@@ -161,16 +199,16 @@ const Home = () => {
           <h3>|</h3>
           <h3><span>Exclusive</span> pricing</h3>
         </div>
-        <div className={classes['cards-section']}>
+        <div className={classes['cards-section']} ref={cardsScope}>
           <div className={classes['cards-group']}>
-            <CategoriesCard imagePath='/optimized-images/ecommerce/models/women/woman-16.webp' headline='Fresh Arrivals' btnText='Shop Now' btnPath='/project-1/items' />
-            <CategoriesCard imagePath='/optimized-images/ecommerce/models/both/both-1.webp' headline='Style Inspiration' btnText='Shop Now' btnPath='/project-1/items' />
-            <CategoriesCard imagePath='/optimized-images/ecommerce/models/women/woman-17.webp' headline='Luxe Lounge' btnText='Shop Now' btnPath='/project-1/items' />
+            <CategoriesCard id='card-1' imagePath='/optimized-images/ecommerce/models/women/woman-16.webp' headline='Fresh Arrivals' btnText='Shop Now' btnPath='/project-1/items' />
+            <CategoriesCard id='card-2' imagePath='/optimized-images/ecommerce/models/both/both-1.webp' headline='Style Inspiration' btnText='Shop Now' btnPath='/project-1/items' />
+            <CategoriesCard id='card-3' imagePath='/optimized-images/ecommerce/models/women/woman-17.webp' headline='Luxe Lounge' btnText='Shop Now' btnPath='/project-1/items' />
           </div>
           <div className={classes['cards-group']}>
-            <CategoriesCard imagePath='/optimized-images/ecommerce/models/men/man-13.webp' headline='Winter Warmers' btnText='Shop Now' btnPath='/project-1/items' />
-            <CategoriesCard imagePath='/optimized-images/ecommerce/models/women/women-2.webp' headline='Fall Essentials' btnText='Shop Now' btnPath='/project-1/items' />
-            <CategoriesCard imagePath='/optimized-images/ecommerce/models/women/woman-15.webp' headline='Summer Ready' btnText='Shop Now' btnPath='/project-1/items' />
+            <CategoriesCard id='card-4' imagePath='/optimized-images/ecommerce/models/men/man-13.webp' headline='Winter Warmers' btnText='Shop Now' btnPath='/project-1/items' />
+            <CategoriesCard id='card-5' imagePath='/optimized-images/ecommerce/models/women/women-2.webp' headline='Fall Essentials' btnText='Shop Now' btnPath='/project-1/items' />
+            <CategoriesCard id='card-6' imagePath='/optimized-images/ecommerce/models/women/woman-15.webp' headline='Summer Ready' btnText='Shop Now' btnPath='/project-1/items' />
           </div>
         </div>
     </div>
