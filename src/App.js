ffbfd050525868193a240fcd2d71261fb37713f0
iefniar <fejeffrey.uv@gmail.com>
2024-06-08 21:00:38 -0300

    travel project started

--- src/App.js
+++ src/App.js
@@ -8,9 +8,8 @@ import IndividualItemPage from "./pages/ecommercePage/IndividualItemPage";
 import ShoppingCartPage from "./pages/ecommercePage/ShoppingCartPage";
 import CheckoutPage from "./pages/ecommercePage/CheckoutPage";
 import Success from "./pages/ecommercePage/Success";
+import TravelHomePage from "./pages/travelPage/TravelHomePage";
 import ScrollToTop from "./components/utilities/ScrollToTop";
-
-import MainContent from './components/MainContent';
 
 const App = () => {
   return (
@@ -26,7 +25,7 @@ const App = () => {
         <Route path="/project-1/cart" element={<ShoppingCartPage />} /> 
         <Route path="/project-1/checkout" element={<CheckoutPage />} /> 
         <Route path="/project-1/success" element={<Success />} /> 
-        <Route path="/project-2" element={<MainContent />} />
+        <Route path="/project-2" element={<TravelHomePage />} />
       </Routes>
     </>
   );
---
+++ src/pages/travelPage/TravelHomePage.js
@@ -0,0 +1,9 @@
+import classes from './TravelHomePage.module.css';
+
+const TravelHomePage = () => {
+  return (
+    <div>TravelHomePage</div>
+  );
+}
+
+export default TravelHomePage;
