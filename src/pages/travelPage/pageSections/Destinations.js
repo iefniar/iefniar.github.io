fd509489e182e7fcf0a4c643ad23ffb90547056b
iefniar <fejeffrey.uv@gmail.com>
2024-06-14 04:40:43 -0300

    travel project destinations section completed

--- src/pages/travelPage/pageSections/Destinations.js
+++ src/pages/travelPage/pageSections/Destinations.js
@@ -6,6 +6,7 @@ import DestinationsCard from '../../../components/DestinationsCard';
 
 const Destinations = (props, ref) => {
     const upperContainerRef = useRef(null);
+    const bottomContainerRef = useRef(null);
     const [yImg1, setYImg1] = useState(0);
     const [yImg2, setYImg2] = useState(0);
 
@@ -57,21 +58,63 @@ const Destinations = (props, ref) => {
                 },
                 '<1'
             )
-            .from('#h2Why', 
+
+            let tl2 = gsap.timeline({
+                defaults: { duration: 1 },
+                scrollTrigger: {
+                    trigger: bottomContainerRef.current,
+                    start: 'top center',
+                    end: 'bottom bottom',
+                    onUpdate: (self) => {
+                        setYImg1(self.progress*100);
+                        setYImg2(self.progress*50);
+                    }
+                }
+            });
+            tl2
+            .fromTo('#subHeader', 
                 {
-                  opacity: 0,
-                  y: '50px'
+                  opacity: 0, 
+                  y: '-50px'
                 },
-                0
-            )
-            .from('#paragraph', 
                 {
-                  opacity: 0,
-                  y: '50px',
-                  stagger: .5
-                },
-                '<'
+                  opacity: 1, 
+                  y: 0
+                }
             )
+            .fromTo('#smallHorizontalLine', 
+                {
+                  opacity: 0, 
+                  width: '50px'
+                },
+                {
+                  opacity: 1, 
+                  width: '100px'
+                },
+                '<1'
+            )
+            .fromTo('#header', 
+                {
+                  opacity: 0, 
+                  y: '-20px'
+                },
+                {
+                  opacity: 1, 
+                  y: 0
+                }
+            )
+            .fromTo('#individualGroup', 
+                {
+                  opacity: 0, 
+                  x: '-50px'
+                },
+                {
+                  opacity: 1, 
+                  x: 0,
+                  stagger: 1
+                }
+            )
+            
         });
 
         return () => ctx.revert();
@@ -97,16 +140,16 @@ const Destinations = (props, ref) => {
                 <DestinationsCard imgPath='/optimized-images/travel/webp/portrait-6.webp' days='7' continent='Asia' headline='Delicious food' description='A beautiful destination to spend the holidays with your family' price='350.00' />
             </div>
         </div>
-        <div className={classes['destinations-bottom-container']}>
-            <div className={classes['sub-header']}>
+        <div className={classes['destinations-bottom-container']} ref={bottomContainerRef}>
+            <div className={classes['sub-header']} id='subHeader'>
                 exceed your expectations
             </div>
-            <div className={classes['small-horizontal-line']} />
-            <div className={classes['header']}>
+            <div className={classes['small-horizontal-line']} id='smallHorizontalLine' />
+            <div className={classes['header']} id='header'>
                 Explore more destinations
             </div>
             <div className={classes['destination-group']}>
-                <div className={classes['individual-group']}>
+                <div className={classes['individual-group']} id='individualGroup'>
                     <div className={classes['img-container']}>
                         <img src='/optimized-images/travel/webp/landscape-1.webp' alt='vacation destination' />
                     </div>
@@ -122,10 +165,7 @@ const Destinations = (props, ref) => {
                         </p>
                     </div>
                 </div>
-                <div className={classes['individual-group']}>
-                    <div className={classes['img-container']}>
-                        <img src='/optimized-images/travel/webp/landscape-14.webp' alt='vacation destination' />
-                    </div>
+                <div className={classes['individual-group']} id='individualGroup'>
                     <div className={classes['content-container']}>
                         <h2>
                             A beautiful destination to spend the holidays with your family
@@ -137,8 +177,11 @@ const Destinations = (props, ref) => {
                             A beautiful destination to spend the holidays with your family. A beautiful destination to spend the holidays with your family.A beautiful destination to spend the holidays with your family.
                         </p>
                     </div>
+                    <div className={classes['img-container']}>
+                        <img src='/optimized-images/travel/webp/landscape-14.webp' alt='vacation destination' />
+                    </div>
                 </div>
-                <div className={classes['individual-group']}>
+                <div className={classes['individual-group']} id='individualGroup'>
                     <div className={classes['img-container']}>
                         <img src='/optimized-images/travel/webp/landscape-4.webp' alt='vacation destination' />
                     </div>
@@ -154,9 +197,11 @@ const Destinations = (props, ref) => {
                         </p>
                     </div>
                 </div>
-                
             </div>
-            
+            <div className={classes['sub-header']}>
+                ...
+            </div>
+            <div className={classes['small-horizontal-line']} />
         </div>
     </div>
   );
