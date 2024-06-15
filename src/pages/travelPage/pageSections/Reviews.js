6f8d67e0ed713e1e99bf09778e1226266c099882
iefniar <fejeffrey.uv@gmail.com>
2024-06-14 21:27:39 -0300

    travel project reviews section started

--- src/pages/travelPage/pageSections/Reviews.js
+++ src/pages/travelPage/pageSections/Reviews.js
@@ -6,57 +6,76 @@ import { reviewsItemsSliderTravelProject } from '../../../Data';
 import { ArrowLeftOutlined, ArrowRightOutlined, StarSharp } from '@mui/icons-material';
 
 const Reviews = (props, ref) => {
+    const [slideIndex, setSlideIndex] = useState(0);
+
+    const handleClick = (direction) => {
+        if(direction === "left"){
+            setSlideIndex(slideIndex > 0 ? slideIndex - 1 : 4);
+        }
+        else{
+            setSlideIndex(slideIndex < 4 ? slideIndex + 1 : 0);
+        }
+    };
+
   return (
     <div className={classes['reviews-container']} ref={ref}>
         <div className={classes['reviews-inner-container']}>
             <div className={classes['slider-container']}>
-                <div className={classes['arrow-left-container']}>
-                    <ArrowLeftOutlined />
-                </div>
-                <div className={classes['slider-wrapper']}>
-                    <div className={classes['slide']}>
-                        <div className='slide-top'>
-                            <div className={classes['customer-area']}>
-                                Sarah Miller
-                            </div>
-                            <div className={classes['stars-area']}>
-                                <div className={classes['star-container']}>
-                                    <StarSharp />
-                                </div>
-                                <div className={classes['star-container']}>
-                                    <StarSharp />
-                                </div>
-                                <div className={classes['star-container']}>
-                                    <StarSharp />
-                                </div>
-                                <div className={classes['star-container']}>
-                                    <StarSharp />
-                                </div>
-                                <div className={classes['star-container']}>
-                                    <StarSharp />
-                                </div>
-                            </div>
-                        </div>
-                        <div className='slide-middle'>
-                            <div className={classes['headline-area']}>
-                                Exceeded Expectations!
-                            </div>
-                            <div className={classes['review-area']}>
-                                <p>
-                                    From the initial consultation to the final farewell, Aujourd'hui Journeys made our dream vacation to Thailand a reality. Their personalized itinerary perfectly captured our desire for relaxation and cultural immersion. We can't wait to book our next adventure with them!
-                                </p>
-                            </div>
-                        </div>
-                        <div className='slide-bottom'>
-                            <div className={classes['date-area']}>
-                                June 7, 2024
-                            </div>
-                        </div>
+                <button className={classes['arrow-left-btn']} onClick={()=>handleClick("left")}>
+                    <div className={classes['arrow-left-inner-container']}>
+                        <ArrowLeftOutlined />
                     </div>
+                </button>
+                <div className={classes['slider-wrapper']} style={{ transform: `translateX(${slideIndex * -100}%)` }} >
+                    {reviewsItemsSliderTravelProject.map(item => (
+                        <div className={classes['slide']} key={item.id}>
+                            <div className={classes['slide-top']}>
+                                <div className={classes['customer-area']}>
+                                    {item.customer}
+                                </div>
+                                <div className={classes['stars-area']}>
+                                    <div className={classes['star-container']}>
+                                        <StarSharp />
+                                    </div>
+                                    <div className={classes['star-container']}>
+                                        <StarSharp />
+                                    </div>
+                                    <div className={classes['star-container']}>
+                                        <StarSharp />
+                                    </div>
+                                    <div className={classes['star-container']}>
+                                        <StarSharp />
+                                    </div>
+                                    <div className={classes['star-container']}>
+                                        <StarSharp />
+                                    </div>
+                                </div>
+                            </div>
+                            <div className='slide-middle'>
+                                <div className={classes['headline-area']}>
+                                    {item.headline}
+                                </div>
+                                <div className={classes['review-area']}>
+                                    <p>
+                                        {item.review}
+                                    </p>
+                                </div>
+                            </div>
+                            <div className='slide-bottom'>
+                                <div className={classes['date-area']}>
+                                    {item.date}
+                                </div>
+                                <div className={classes['small-horizontal-line']} />
+                            </div>
+                        </div>
+                    ))}
+                    
                 </div>
-                <div className={classes['arrow-right-container']}>
-                    <ArrowRightOutlined />
-                </div>
+                <button className={classes['arrow-right-btn']} onClick={()=>handleClick("right")}>
+                    <div className={classes['arrow-right-inner-container']}>
+                        <ArrowRightOutlined /> 
+                    </div>
+                </button>
             </div>
 
         </div>
